46b43adecd4d5ee6351168b441bacf70f8ec79b6
EC2 Default User <ec2-user@ip-10-0-10-40.ap-northeast-1.compute.internal>
2019-09-01 09:04:04 +0000

    add KDF injest functionality

---
+++ injest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+endpoint=https://62pu6qnh23.execute-api.ap-northeast-1.amazonaws.com/prod/metrics
+
+while :; do
+  v1=$(($RANDOM & 100))
+  v2=$(($RANDOM & 100))
+  v3=$(($RANDOM & 100))
+  v4=$(($RANDOM & 100))
+  curl -d "{\"firstName\": \"John\", \"lastName\": \"Doe\", \"values\" : \"$v1,$v2,$v3,$v4\" }" $endpoint
+  sleep 60
+done
