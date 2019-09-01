7949455efdddc391e7790a7f178eb579ce61e647
EC2 Default User <ec2-user@ip-10-0-10-40.ap-northeast-1.compute.internal>
2019-09-01 03:41:37 +0000

    init - simple Lambda

---
+++ src/main/java/example/ResponseClass.java
@@ -0,0 +1,16 @@
+package example;
+        
+public class ResponseClass {
+    String greetings;
+    public String getGreetings() {
+        return greetings;
+    }
+    public void setGreetings(String greetings) {
+        this.greetings = greetings;
+    }
+    public ResponseClass(String greetings) {
+        this.greetings = greetings;
+    }
+    public ResponseClass() {
+    }
+}
